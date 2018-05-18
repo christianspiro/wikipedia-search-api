5b61ed58b515baf1c789bf29318e52761b77b1aa
Christian Spiro <christianspiro1@gmail.com>
2018-05-18 18:40:13 -0400

    added background image, functional now on local tests

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 $(document).ready(() => {
-  $("#search-btn").click(() => {
+  $('#search-btn').click(() => {
     let searchTerm = $("#search-query").val();
-    let url =
-      "https://en.wikipedia.org/w/api.php?action=opensearch=" +
+    let api =
+      "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
       searchTerm +
       "&format=json&callback=?";
 
     $.ajax({
       type: "GET",
-      url: url,
+      url: api,
       async: false,
       dataType: "json",
       success: (data) => {
-        console.log(data);
+        for (i=0; i < data[1].length;i++) {
+          $('.output').prepend('<li><a href= '+data[3][i]+'>'+data[1][i] +'</a><p>'+data[2][i]+ '</p></li>');
+          $('#search-query').val('');
+        }
       },
       error: () => {
-        alert("Not Supported");
+        alert('Not Supported');
       }
     });
   });
