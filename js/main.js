69d73994e5abeb5394010c245ecb2498e5f2cb57
christianspiro <christianspiro1@gmail.com>
2018-07-21 10:10:57 -0400

    Now to fix the images

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 //Using jQuery To Operate the AJAX call
 const colors = chroma.scale(['#fafa6e','#2A4858'])
-        .mode('lch').colors(10);
-let searchTerm = $("#search-query").val();
-  $('#search-btn').click(() => {
-    let backValue = Math.floor((Math.random() * 10) + 1);
-    $("#search-btn").css("background-color",backValue);
+    .mode('lch').colors(10);
+
+$('#search-btn').click(() => {
+    let searchTerm = $("#search-query").val();
     let api =
-      "https://en.wikipedia.org/w/api.php?action=opensearch&search="+
-      searchTerm +
-      "&format=json&callback=?";
+        `https://en.wikipedia.org/w/api.php?action=opensearch&search=
+        ${searchTerm} 
+        &format=json&callback=?`;
 
     $.ajax({
-      type: "GET",
-      url: api,
-      async: false,
-      dataType: "json",
-      success: (data) => {
-        for (i=0; i < data[1].length;i++) {
-          $('.output').prepend(
-          `
+        type: "GET",
+        url: api,
+        async: false,
+        dataType: "json",
+        success: (data) => {
+            console.log(data);
+            for (i=0; i < data[1].length;i++) {
+                $('.output').prepend(
+                    `
 <li class="search-result"></li><div class="row">
     <div class="col s12 m12">
       <div class="card">
@@ -35,42 +35,38 @@ let searchTerm = $("#search-query").val();
       </div>
     </div>
   </div></li>`);
-          $('#search-query').val('');
+                $('#search-query').val('');
 
+            }
+        },
+        error: () => {
+            alert('Not Supported');
         }
-      },
-      error: () => {
-        alert('Not Supported');
-      }
+
 
     });
 
-  });
 
-$.ajax({
-    url: `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=250&pilimit=20&wbptterms=description&gpssearch=${searchTerm}&gpslimit=20`,
-    method: "GET",
-    dataType: "json",
-    success: (newData) => {
-        console.log('newData');
-        for (let i = 0; i < 20; i++) {
-            if (newData.query.pages[i].hasOwnProperty("thumbnail") === true) {
-                $("#image" + (newData.query.pages[i].index - 1)).html(`<img src='${newData.query.pages[i].thumbnail.source}' class='responsive-img valign'>`);
-            } else {
-                $("#image" + (newData.query.pages[i].index - 1)).html("<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Article_icon_cropped.svg/512px-Article_icon_cropped.svg.png' class='responsive-img valign articleIcon'>");
-            }
-        }
-    },
-    error: function() {
-        console.log("second call unsuccessful");
-    }
 });
 
 
+
 //Function to enable enter on search
-    document.querySelector("#search-query").addEventListener("keyup", event => {
-        if(event.key !== "Enter") return;
-        document.querySelector("#search-btn").click();
-        event.preventDefault();
-    });
+document.querySelector("#search-query").addEventListener("keyup", event => {
+    if(event.key !== "Enter") return;
+    document.querySelector("#search-btn").click();
+    event.preventDefault();
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
